6551e61258f6ccd7883754a0932a1a15b2f09e3b
Goldcoders <goldcoders@protonmail.com>
2021-06-07 02:33:50 +0800

    update postcss

--- assets/css/postcss.config.js
+++ assets/css/postcss.config.js
@@ -1,7 +1,25 @@
-const themeDir = __dirname + "/../../";
+const dir = __dirname;
 
-module.exports = {
-    plugins: [
-        require("tailwindcss")(themeDir + "assets/css/tailwind.config.js"),
+const purgecss = require('@fullhuman/postcss-purgecss')({
+    // see https://gohugo.io/hugo-pipes/postprocess/#css-purging-with-postcss
+    content: [
+        './hugo_stats.json',
     ],
-};
+    defaultExtractor: (content) => {
+        let els = JSON.parse(content).htmlElements;
+        return els.tags.concat(els.classes, els.ids);
+    }
+})
+
+module.exports = {    
+    plugins: [        
+        require('postcss-import')({
+            path: [dir]
+            }), 
+        require('tailwindcss')('assets/css/tailwind.config.js'),
+        require('autoprefixer')({
+            path: [dir]
+        }),
+        ...(process.env.HUGO_ENVIRONMENT === 'production' ? [purgecss] : [])
+    ]
+}
